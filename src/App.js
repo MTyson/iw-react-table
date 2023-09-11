c19e587e5835e209e96bbe41c3966b168ad00c60
Matthew Tyson <matthewcarltyson@gmail.com>
2023-09-11 17:58:43 +0000

    example table

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CryptoTable from './CryptoTable4';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Crypto Data</h1>
+      <CryptoTable />
+    </div>
+  );
+}
+
+export default App;
